b5340dfe32025fcc9fd1be0c5f6772524258390d
Pranjal Khare <pranjalkhare2004@gmail.com>
2025-03-21 22:32:33 +0530

    project part 1 completed

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,38 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
-
+import dotenv from "dotenv";
+import path from "path";
 import passport from "passport";
 import session from "express-session";
-import ConnectMongoDBSession from "connect-mongodb-session";
+import connectMongo from "connect-mongodb-session";
 
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
 import { buildContext } from "graphql-passport";
-
-import { connectDB } from "./db/connectDB.js";
-
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
-import { configDotenv } from "dotenv";
+
+import { connectDB } from "./db/connectDB.js";
 import { configurePassport } from "./passport/passport.config.js";
 
-configDotenv();
+import job from "./cron.js";
 
+dotenv.config();
 configurePassport();
 
+job.start();
+const __dirname = path.resolve();
 const app = express();
 
 const httpServer = http.createServer(app);
 
+const MongoDBStore = connectMongo(session);
 
-const store = new ConnectMongoDBSession({
+const store = new MongoDBStore({
 	uri: process.env.MONGO_URI,
 	collection: "sessions",
 });
@@ -41,7 +45,7 @@ app.use(
 		resave: false, // this option specifies whether to save the session to the store on every request
 		saveUninitialized: false, // option specifies whether to save uninitialized sessions
 		cookie: {
-			maxAge: 1000 * 60 * 60 * 24 * 7, // expires in 1 week
+			maxAge: 1000 * 60 * 60 * 24 * 7,
 			httpOnly: true, // this option prevents the Cross-Site Scripting (XSS) attacks
 		},
 		store: store,
@@ -57,6 +61,7 @@ const server = new ApolloServer({
 	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
+// Ensure we wait for our server to start
 await server.start();
 
 // Set up our Express middleware to handle CORS, body parsing,
@@ -64,7 +69,6 @@ await server.start();
 app.use(
 	"/graphql",
 	cors({
-		
 		origin: "http://localhost:3000",
 		credentials: true,
 	}),
@@ -72,11 +76,15 @@ app.use(
 	// expressMiddleware accepts the same arguments:
 	// an Apollo Server instance and optional configuration options
 	expressMiddleware(server, {
-		context: async ({ req, res }) => buildContext({  req, res }),
-	}),
+		context: async ({ req, res }) => buildContext({ req, res }),
+	})
 );
 
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
 
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
